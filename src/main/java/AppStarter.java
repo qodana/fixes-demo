c20aee7b676af9a232af96b9ba38d094c3912212
tiulpin <viktor@tiulp.in>
2023-05-05 15:22:52 +0200

    Pour some code

---
+++ src/main/java/AppStarter.java
@@ -0,0 +1,14 @@
+@SuppressWarnings("unused")
+public class BadExceptionNameClass extends RuntimeException {
+}
+
+public class AppStarter {
+    public static void main(String[] args) {
+        int a ;
+        int b = a;
+        if(true) {
+
+        }
+        return;
+    }
+}
